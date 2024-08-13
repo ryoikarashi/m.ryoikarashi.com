a40a57b8928f020645cc3f447cc57461c4d30f45
RYO IKARASHI <ryo.5630@gmail.com>
2024-08-13 13:57:55 +0900

    add favicon

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,7 @@ import { EleventyRenderPlugin } from "@11ty/eleventy";
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPassthroughCopy({ 'src/assets/favicon': '/' });
 
   return {
     dir: {
