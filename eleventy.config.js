9141700ed66d6cb1cb601378a04f95ca74566740
RYO IKARASHI <ryo.5630@gmail.com>
2024-11-09 18:13:15 +0900

    add sitemap.xml

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,11 @@ import { EleventyRenderPlugin } from "@11ty/eleventy";
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
-  eleventyConfig.addPassthroughCopy({ 'src/assets/favicon': '/' });
+  eleventyConfig.addGlobalData("site", {
+    url: "https://m.ryoikarashi.com",
+  });
+  eleventyConfig.addPassthroughCopy({ 'src/assets/favicon': '/' })
+  eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' })
 
   return {
     dir: {
